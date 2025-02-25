b1c1e00f22b94677e7c53ab029a944649645ea9d
Aahladee <aahladeedalal@gmail.com>
2025-02-25 21:55:08 +0530

    change in background

--- src/screens/LoginScreeen.tsx
+++ src/screens/LoginScreeen.tsx
@@ -42,7 +42,7 @@ const LoginScreen = ({ navigation }: Props) => {
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", padding: 20 },
+  container: { flex: 1, justifyContent: "center", padding: 20, backgroundColor:"#ffffff" },
   title: { fontSize: 24, fontWeight: "bold", textAlign: "center", marginBottom: 20 },
   input: { borderWidth: 1, padding: 10, marginBottom: 10, borderRadius: 5 },
 });
